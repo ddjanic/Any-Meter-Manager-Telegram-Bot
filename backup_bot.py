bfd4e9179e221cc34a97ffcee16cd62031050489
Nikita Bukhvostov <ddjanic@yandex.ru>
2024-05-30 10:50:10 +0300

    data backup before add func() NPU

---
+++ backup_bot.py
@@ -0,0 +1,240 @@
+from math import e
+import random
+import os
+import cv2
+import numpy as np
+import qrcode
+from scipy.datasets import face
+import telebot
+from telebot import types
+from src import filters
+# from style_transfer import styleTransfer
+import config
+from dotenv import load_dotenv
+from src.face_parts_detector import FacePartsDetector
+# date time stamp
+from datetime import datetime
+
+load_dotenv()
+
+# date time stamp
+now = datetime.now() # current date and time
+date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
+
+#Token = os.getenv("Token")
+bot = telebot.TeleBot("7088157183:AAHW9mQ_iWKdUmiijz6nlBYmoiZtIteX6Os")
+
+markup = types.ReplyKeyboardMarkup(row_width=4)
+itembtn1 = types.KeyboardButton('+')
+itembtn2 = types.KeyboardButton('-')
+itembtn3 = types.KeyboardButton('*')
+itembtn4 = types.KeyboardButton('/')
+markup.add(itembtn1, itembtn2, itembtn3, itembtn4)
+
+bot_state = None
+content_image_path = None
+
+
+face_parts_detector = FacePartsDetector()
+
+
+@bot.message_handler(commands=['start'])
+def send_start(message):
+    bot.send_message(message.chat.id, config.start_test)
+
+
+@bot.message_handler(commands=['help'])
+def send_help(message):
+    bot.send_message(message.chat.id, config.help_text)
+
+
+@bot.message_handler(commands=['pencil_sketch'])
+def pencil_sketch_image(message):
+    bot.send_message(message.chat.id, "Send me an image")
+
+
+@bot.message_handler(content_types=['photo'])
+def pencil_sketch_image_next_step(message):
+    image = message_photo_to_image(message)
+    image_result = filters.image2pencilSketch(image)
+    photo = image_to_message_photo(image_result)
+    if photo is not None:
+        bot.send_photo(message.chat.id, photo)
+    else:
+        bot.send_message(message.chat.id, "Something went wrong 😭")
+
+
+@bot.message_handler(commands=['gray'])
+def gray_image(message):
+    bot.send_message(message.chat.id, "Send me an image")
+    bot.register_next_step_handler(message, gray_image_next_step)
+
+
+@bot.message_handler(content_types=['photo'])
+def gray_image_next_step(message):
+    if message.content_type == 'photo':
+        image = message_photo_to_image(message)
+        image_result = filters.image2gray(image)
+        photo = image_to_message_photo(image_result)
+        if photo is not None:
+            #bot.send_photo(message.chat.id, photo)
+            bot.send_photo(message.chat.id, 
+                           photo, 
+                           reply_to_message_id=message.message_id,
+                           caption='Вода: показания на - '+date_time)
+        else:
+            bot.send_message(message.chat.id, "Something went wrong 😭")
+
+###### 
+# water meter recognition
+######
+@bot.message_handler(commands=['water'])
+def water_image(message):
+    bot.send_message(message.chat.id, "Пришлите изображение/фото счётчика для снятия показаний")
+    bot.register_next_step_handler(message, water_image_next_step)
+
+###### 
+# water meter recognition
+######
+@bot.message_handler(content_types=['photo'])
+def water_image_next_step(message):
+    if message.content_type == 'photo':
+        image = message_photo_to_image(message)
+        image_result = filters.image2gray(image)
+        photo = image_to_message_photo(image_result)
+        if photo is not None:
+            #bot.send_photo(message.chat.id, photo)
+            bot.send_photo(message.chat.id, 
+                           photo, 
+                           reply_to_message_id=message.message_id,
+                           caption='Вода: показания на - '+date_time)
+        else:
+            bot.send_message(message.chat.id, "Что-то пошло не так 😭")
+
+@bot.message_handler(commands=['besco'])
+def send_pencil_sketch(message):
+    bot.send_message(message.chat.id, "Send me an image of your beautiful face")
+    bot_state = 'besco'
+
+
+@bot.message_handler(commands=['cartoon'])
+def send_pencil_sketch(message):
+    bot.send_message(message.chat.id, "Send me an image")
+    bot_state = 'cartoon'
+
+
+@bot.message_handler(commands=['inverse'])
+def send_pencil_sketch(message):
+    bot.send_message(message.chat.id, "Send me an image")
+    bot_state = 'inverse'
+
+
+@bot.message_handler(commands=['qrcode'])
+def send_qrcode(message):
+    msg = bot.send_message(message.chat.id, "Send me a text or url")
+    # bot_state = 'qrcode'
+    bot.register_next_step_handler(msg, process_qrcode_step)
+
+
+@bot.message_handler(commands=['qrcode'])
+def face_puzzle(message):
+    msg = bot.send_message(message.chat.id, "Send me a text or url")
+    # bot_state = 'qrcode'
+    bot.register_next_step_handler(msg, process_qrcode_step)
+
+
+@bot.message_handler(commands=['style_transfer'])
+def send_style_transfer(message):
+    photo = open('input/style_transfer.png', "rb")
+    bot.send_photo(message.chat.id, photo)
+    bot.send_message(message.chat.id, "Send me content image")
+    bot_state = 'style_transfer_1'
+
+
+@bot.message_handler(commands=['who_knows_me_best'])
+def who_knows_me_best(message):
+    bot.send_message(message.chat.id, "Send me your face image")
+    bot.register_next_step_handler(message, who_knows_me_best_next_step)
+
+
+@bot.message_handler(content_types=['photo'])
+def who_knows_me_best_next_step(message):
+    if message.content_type == 'photo':
+        image = message_photo_to_image(message)
+        image_result = filters.who_knows_me_best(image, face_parts_detector)
+        photo = image_to_message_photo(image_result)
+        if photo is not None:
+            bot.send_photo(message.chat.id, photo)
+        else:
+            bot.send_message(message.chat.id, "Something went wrong 😭")
+
+
+def message_photo_to_image(message):
+    file_info = bot.get_file(message.photo[-1].file_id)
+    downloaded_file = bot.download_file(file_info.file_path)
+    file_data = np.frombuffer(downloaded_file, dtype=np.uint8)
+    image = cv2.imdecode(file_data, 1)
+    return image
+
+
+def image_to_message_photo(image):
+    retval, buffer = cv2.imencode(".png", image)
+    if retval:
+        return buffer
+    else:
+        return None
+
+
+@bot.message_handler(content_types=['photo'])
+def send_photo(message):
+    global bot_state, content_image_path
+
+    file_info = bot.get_file(message.photo[-1].file_id)
+
+    if bot_state == 'style_transfer_1':
+        content_image_path = save_image(file_info, 'content_image')
+        bot.send_message(message.chat.id, "Send me style image")
+        bot_state = 'style_transfer_2'
+        return
+
+    elif bot_state == 'style_transfer_2':
+        style_image_path = save_image(file_info, 'style_image')
+        image_result = styleTransfer(content_image_path, style_image_path)
+
+    elif bot_state == 'besco':
+        image_path = save_image(file_info)
+        image_result = face_eyes_lips(image_path)
+
+    elif bot_state == 'cartoon':
+        image_path = save_image(file_info)
+        image_result = image2cartoon(image_path)
+
+    image_path = os.path.join('output', file_info.file_path)
+    cv2.imwrite(image_path, image_result)
+
+    photo = open(image_path, "rb")
+    bot.send_photo(message.chat.id, photo)
+
+
+@bot.message_handler(func=lambda message: True)
+def send_message(message):
+    try:
+        pass
+
+    except:
+        bot.send_message(message.chat.id, "Something went wrong 😭")
+
+
+def process_qrcode_step(message):
+    try:
+        image_path = 'output/photos/qrcode.png'
+        img = qrcode.make(message.text)
+        img.save(image_path)
+
+        photo = open(image_path, "rb")
+        bot.send_photo(message.chat.id, photo)
+    except Exception as e:
+        bot.send_message(message.chat.id, "Something went wrong 😭")
+
+
+bot.polling(none_stop=True)
---
+++ src/backup_filters.py
@@ -0,0 +1,138 @@
+import numpy as np
+import cv2
+from src.landmarks_detector import LandmarksDetector
+
+
+def dodge(x, y):
+    return cv2.divide(x, 255 - y, scale=256)
+
+
+def burn(image, mask):
+    return 255 - cv2.divide(255 - image, 255 - mask, scale=256)
+
+
+def image2pencilSketch(image):
+    image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    image_gray_inv = 255 - image_gray
+    image_gray_inv_blur = cv2.GaussianBlur(image_gray_inv, (21, 21), sigmaX=0, sigmaY=0)
+    image_dodged = dodge(image_gray, image_gray_inv_blur)
+    image_result = burn(image_dodged, image_gray_inv_blur)
+    return image_result
+
+
+def image2gray(image):
+    image_result = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    return image_result
+
+
+def landmarks2image(image, background, landmarks):
+    mask = np.zeros(image.shape[:2], np.uint8)
+    cv2.drawContours(mask, [landmarks], -1, (255, 255, 255), -1)
+
+    mask = cv2.GaussianBlur(mask, (9, 9), 0)
+    mask = cv2.multiply(cv2.subtract(mask, 50), 2)
+
+    r_min = np.min(landmarks[:, 1])
+    r_max = np.max(landmarks[:, 1])
+    c_min = np.min(landmarks[:, 0])
+    c_max = np.max(landmarks[:, 0])
+    r_center = (r_max + r_min) // 2
+    c_center = (c_max + c_min) // 2
+
+    image_lips = image[r_min:r_max, c_min:c_max]
+    mask_lips = mask[r_min:r_max, c_min:c_max]
+
+    lips = cv2.bitwise_and(image_lips, image_lips, mask=mask_lips)
+    lips = cv2.resize(lips, (0, 0), fx=2, fy=2)
+    mask_lips = cv2.resize(mask_lips, (0, 0), fx=2, fy=2)
+
+    lips_w, lips_h, _ = lips.shape
+    lips_x_min = r_center - lips_w // 2
+    lips_y_min = c_center - lips_h // 2
+
+    mask_new = np.zeros(image.shape[:2], dtype=np.uint8)
+    mask_new[lips_x_min:lips_x_min + lips_w, lips_y_min:lips_y_min + lips_h] = mask_lips
+
+    lips_new = np.zeros(image.shape, dtype=np.uint8)
+    lips_new[lips_x_min:lips_x_min + lips_w, lips_y_min:lips_y_min + lips_h] = lips
+    foreground = lips_new.astype(float)  # Convert uint8 to float
+
+    # Normalize the alpha mask to keep intensity between 0 and 1
+    alpha = mask_new.astype(float) / 255
+    alpha = cv2.merge((alpha, alpha, alpha))
+
+    foreground = cv2.multiply(alpha, foreground)
+    background = cv2.multiply(1.0 - alpha, background)
+    result = cv2.add(foreground, background)
+
+    return result
+
+
+def face_eyes_lips(image_path):
+    image = cv2.imread(image_path)
+    background = image.astype(float)
+    landmarks_detector = LandmarksDetector()
+    all_face_landmarks = landmarks_detector.get_landmarks(image)
+    lips_landmarks_indexes = [52, 64, 63, 71, 67, 68, 61, 58, 59, 53, 56, 55]
+    left_eye_landmarks_indexes = [35, 41, 40, 42, 39, 37, 33, 36]
+    right_eye_landmarks_indexes = [89, 95, 94, 96, 93, 91, 87, 90]
+
+    for face_landmarks in all_face_landmarks:
+        left_eye_landmarks = np.array([face_landmarks[i] for i in left_eye_landmarks_indexes])
+        background = landmarks2image(image, background, left_eye_landmarks)
+
+        right_eye_landmarks = np.array([face_landmarks[i] for i in right_eye_landmarks_indexes])
+        background = landmarks2image(image, background, right_eye_landmarks)
+
+        lips_landmarks = np.array([face_landmarks[i] for i in lips_landmarks_indexes])
+        background = landmarks2image(image, background, lips_landmarks)
+
+    result = background.astype(np.uint8)
+    return result
+
+
+def image2cartoon(image_path):
+    image = cv2.imread(image_path)
+    image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    image_gray_blur = cv2.medianBlur(image_gray, 5)
+    edges = cv2.adaptiveThreshold(image_gray_blur, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 7, 7)
+    image_color = cv2.bilateralFilter(image, 9, 250, 250)
+    image_result = cv2.bitwise_and(image_color, image_color, mask=edges)
+    return image_result
+
+
+def who_knows_me_best(image, face_parts_detector):
+    embedding, aligned_image, left_eye_image, right_eye_image, nose_image, lips_image = face_parts_detector(image)
+
+    # cv2.imwrite('output/aligned_image.jpg', aligned_image)
+    # cv2.imwrite('output/left_eye_image.jpg', left_eye_image)
+    # cv2.imwrite('output/right_eye_image.jpg', right_eye_image)
+    # cv2.imwrite('output/nose_image.jpg', nose_image)
+    # cv2.imwrite('output/lips_image.jpg', lips_image)
+
+    face_dataset = np.load('face_dataset.npy', allow_pickle=True)
+    face_embedding = [face['embedding'] for face in face_dataset]
+    distances = np.linalg.norm(face_embedding - embedding, axis=1)
+    nearest_faces_indices = distances.argsort()[:3]
+
+    for i in nearest_faces_indices:
+        face = face_dataset[i]
+        image_path = face['image_path']
+        image = cv2.imread(image_path)
+        embedding, aligned_image, left_eye_image, right_eye_image, nose_image, lips_image = face_parts_detector(image)
+
+        # cv2.imwrite(f'output/aligned_image_{i}.jpg', aligned_image)
+        # cv2.imwrite(f'output/left_eye_image_{i}.jpg', left_eye_image)
+        # cv2.imwrite(f'output/right_eye_image_{i}.jpg', right_eye_image)
+        # cv2.imwrite(f'output/nose_image_{i}.jpg', nose_image)
+        # cv2.imwrite(f'output/lips_image_{i}.jpg', lips_image)
+
+
+if __name__ == "__main__":
+    image_path = "input/photos/file_0.jpg"
+    # result = image2cartoon(image_path)
+    # result = image2gray(image_path)
+    # result = image2pencilSketch(image_path)
+    result = face_eyes_lips(image_path)
+    cv2.imshow('output', result)
+    cv2.waitKey(0)
